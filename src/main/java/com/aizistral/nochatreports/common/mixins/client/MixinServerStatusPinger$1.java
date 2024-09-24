f47c08bbc09f9c84be28bac905c2641e879ed942
pietro-lopes <97140255+pietro-lopes@users.noreply.github.com>
2024-09-23 21:17:42 -0300

    Fixes #481

--- src/main/java/com/aizistral/nochatreports/common/mixins/client/MixinServerStatusPinger$1.java
+++ src/main/java/com/aizistral/nochatreports/common/mixins/client/MixinServerStatusPinger$1.java
@@ -1,8 +1,12 @@
 package com.aizistral.nochatreports.common.mixins.client;
 
-import java.lang.reflect.Field;
+import java.net.InetSocketAddress;
 
+import net.minecraft.client.multiplayer.ServerStatusPinger;
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
+import net.minecraft.network.Connection;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,7 +16,6 @@ import com.aizistral.nochatreports.common.config.NCRConfig;
 import com.aizistral.nochatreports.common.core.ServerDataExtension;
 import com.aizistral.nochatreports.common.platform.extensions.ServerPingerExtension;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.network.protocol.status.ClientboundStatusResponsePacket;
 import net.minecraft.network.protocol.status.ServerStatus;
@@ -27,13 +30,20 @@ public abstract class MixinServerStatusPinger$1 implements ServerPingerExtension
 	 * @author Aizistral (current version)
 	 */
 
+	@Unique
+	private ServerDataExtension nochatreports$serverData;
+
+	@Inject(method = "<init>", at = @At("RETURN"))
+	private void captureServerData(ServerStatusPinger serverStatusPinger, Connection connection, ServerData serverData, Runnable runnable, Runnable runnable2, InetSocketAddress inetSocketAddress, ServerAddress serverAddress, CallbackInfo ci){
+		nochatreports$serverData = (ServerDataExtension) serverData;
+	}
+
 	@Inject(method = "handleStatusResponse(Lnet/minecraft/network/protocol/status/ClientboundStatusResponsePacket;)V",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/status/ServerStatus;"
-					+ "description()Lnet/minecraft/network/chat/Component;",
-					ordinal = 0, shift = At.Shift.BEFORE))
+					+ "description()Lnet/minecraft/network/chat/Component;"))
 	private void getNoChatReports(ClientboundStatusResponsePacket packet, CallbackInfo info) {
 		boolean preventsReports = ((ServerDataExtension) (Object) packet.status()).preventsChatReports();
-		((ServerDataExtension) this.getServerData()).setPreventsChatReports(preventsReports);
+		nochatreports$serverData.setPreventsChatReports(preventsReports);
 
 		if (NCRConfig.getCommon().enableDebugLog()) {
 			NCRCore.LOGGER.info("Received status response packet from server, preventsChatReports: {}",
